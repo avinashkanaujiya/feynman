0697c50b5a9f1dd42d774466d8ebca87a9f2a626
avinashkanaujiya <github@avinashkanaujiya.me>
2022-07-09 14:39:23 +0530

    adding sw build file

---
+++ build-sw.js
@@ -0,0 +1,12 @@
+// build-sw.js
+import {generateSW} from 'workbox-build';
+
+generateSW({
+  swDest: './docs/sw.js',
+  globDirectory: './docs',
+  globPatterns: [
+    '**',
+    '*',
+    '*'
+  ]
+});
