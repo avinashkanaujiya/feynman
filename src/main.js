a58b93821bac45ade2501ed18f8b5ae4dfc8b6f3
avinashkanaujiya <github@avinashkanaujiya.me>
2022-07-09 11:57:08 +0530

    first working proto

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import { createStore } from 'vuex'
+import App from './App.vue'
+import router from './router'
+import './registerServiceWorker'
+
+const app = createApp(App)
+const store = createStore({
+  state () {
+    return {
+      count: 10
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+app.use(router);
+app.use(store);
+app.mount('#app');
