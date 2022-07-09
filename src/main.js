cce916497112795a83e076f93f769d43f29f6b45
avinashkanaujiya <github@avinashkanaujiya.me>
2022-07-09 20:51:07 +0530

    updated images

--- src/main.js
+++ src/main.js
@@ -2,18 +2,22 @@ import { createApp } from 'vue'
 import { createStore } from 'vuex'
 import App from './App.vue'
 import router from './router'
+
 const prod = process.env.NODE_ENV === 'production'
 const shouldSW = 'serviceWorker' in navigator && prod
 const shouldSWDev = 'serviceWorker' in navigator && !prod
+
 if (shouldSW) {
   navigator.serviceWorker.register('/sw.js').then(() => {
     console.log("Service Worker Registered!")
   })
 } else if (shouldSWDev) {
   navigator.serviceWorker.register('/sw-dev.js').then(() => {
-    console.log('Service Worker Registered!')
+    console.log('Development Service Worker Registered!')
   })
 }
+
+
 const app = createApp(App)
 const store = createStore({
   state () {
